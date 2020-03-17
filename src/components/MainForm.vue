674b03251f88b5dc16ec6518c551cf12c2a700b3
1123690984 <1123690984@qq.com>
2020-03-17 23:23:13 +0800

    1

---
+++ src/components/MainForm.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+ <v-row>
+      <v-col cols="4">
+        <v-subheader></v-subheader>
+      </v-col>
+      <v-col cols="8">
+        <v-text-field
+          label="Amount"
+          value="10.00"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+
+    </div>
+</template>
